c474a45ede4528384103d6240195508d81f97b72
Jonas Frid <jonas.frid@avaloninnovation.com>
2019-02-22 10:38:09 +0100

    Tests

--- app/src/test/java/se/idoapps/kotlinmotorcycles/unit/viewmodel/EditMotorcycleViewModelTests.kt
+++ app/src/test/java/se/idoapps/kotlinmotorcycles/unit/viewmodel/EditMotorcycleViewModelTests.kt
@@ -8,12 +8,14 @@ import org.junit.Test
 import org.junit.rules.TestRule
 import org.mockito.Mockito
 import se.idoapps.kotlinmotorcycles.model.*
+import se.idoapps.kotlinmotorcycles.service.AnalyticsServiceInterface
 import se.idoapps.kotlinmotorcycles.service.WebServiceInterface
 import se.idoapps.kotlinmotorcycles.viewmodel.*
 
 class EditMotorcycleViewModelTests {
     private lateinit var viewModel: EditMotorcycleViewModelInterface
     private lateinit var webservice: WebServiceInterface
+    private lateinit var analytics: AnalyticsServiceInterface
 
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
@@ -21,7 +23,8 @@ class EditMotorcycleViewModelTests {
     @Before
     fun initTests() {
         webservice = Mockito.mock(WebServiceInterface::class.java)
-        viewModel = EditMotorcycleViewModel(webservice)
+        analytics = Mockito.mock(AnalyticsServiceInterface::class.java)
+        viewModel = EditMotorcycleViewModel(webservice, analytics)
     }
 
     @Test
